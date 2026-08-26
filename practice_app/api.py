52c9d4e6e8504c9faacca2852ad9cbe16ebbff0f
Thennarasu27 <thennarasum2705@gmail.com>
2026-08-26 08:46:59 +0000

    2

--- practice_app/api.py
+++ practice_app/api.py
@@ -3,12 +3,58 @@ import frappe
 @frappe.whitelist()
 def hello():
     return "Hello"
-
 @frappe.whitelist()
 def custom_hello():
     return "Hi"
-
-
 @frappe.whitelist()
 def hookfunc(doc,method):
     frappe.msgprint("Hook executed!")
+
+
+
+#assignment
+@frappe.whitelist()
+def func1():
+    Book = frappe.qb.DocType("Book")
+    Author = frappe.qb.DocType("Author")
+    books = (
+        frappe.qb.from_(Book)
+        .join(Author)
+        .on(Book.author == Author.name)
+        .select(
+            Book.title,
+            Book.status,
+            Author.author_name
+        )
+        .limit(3)
+    )
+    results = books.run(as_dict=True)
+    return results
+
+@frappe.whitelist()
+def func2():
+    doc = frappe.get_doc("Book", "1")
+    doc.status = "Issued"
+    doc.save()
+    frappe.db.commit()
+    return {
+        "name": doc.name,
+        "title": doc.title,
+        "status": doc.status
+    }
+
+@frappe.whitelist()
+def func3():
+    books = frappe.get_all(
+        "Book",
+        fields=["name"]
+    )
+    for book in books:
+        frappe.db.set_value(
+            "Book",
+            book.name,
+            "status",
+            "Available"
+        )
+        frappe.db.commit()
+    return "Bulk updated - books updated"
